1b0320ffc8bc56bcf08cd3e98f66c089ceaf43a0
Jakub Nenczak <kubanenczak@gmail.com>
2024-11-02 17:06:09 +0100

    Moved prompt definition to minishell.h

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jnenczak <jnenczak@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 20:07:21 by jnenczak          #+#    #+#             */
-/*   Updated: 2024/11/02 16:10:33 by jnenczak         ###   ########.fr       */
+/*   Updated: 2024/11/02 17:05:01 by jnenczak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 # include <parsing.h>
 # define DOUBLE_SPECIAL_CHARSET "|&<>"
 # define SINGLE_SPECIAL_CHARSET "="
+# define PROMPT "minishell > "
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,16 +3,22 @@
 int	main(void)
 {
 	char	*line;
+	char	**sentences;
+
 	while (true)
 	{
-		line = readline("minishell$ ");
+		line = readline(PROMPT);
 		if (line == NULL)
 		{
 			ft_putchar_fd('\n', 1);
 			break ;
 		}
-		if (ft_is_whitespace(line) == false)
-			ft_print_enhanced_split(ft_split_sentences(line));
+		if (!ft_is_whitespace(line))
+		{
+			sentences = ft_split_sentences(line);
+			ft_print_enhanced_split(sentences);
+			free(sentences);
+		}
 		free(line);
 	}
 	return (0);
