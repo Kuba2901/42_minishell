cebd53a8453f564882a300644224a6fbb84c8b63
Jakub Nenczak <kubanenczak@gmail.com>
2025-02-14 10:11:37 +0100

    Handling multiple -n flags

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,46 +6,61 @@
 /*   By: jnenczak <jnenczak@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 22:54:32 by jnenczak          #+#    #+#             */
-/*   Updated: 2025/02/13 23:21:36 by jnenczak         ###   ########.fr       */
+/*   Updated: 2025/02/14 09:10:30 by jnenczak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <builtins.h>
 #include <minishell.h>
 
-void	builtin_echo(t_shell *shell, t_ast_node *node)
+static size_t	_determine_first_arg(t_ast_node *node)
 {
-	t_bool	newline;
 	char	**args;
 	size_t	i;
-	char	*arg;
+	size_t	j;
+	char	c;
 
 	args = node->token_node->args;
-	newline = true;
-	if (args && args[1] && args[1][0] == '-')
+	i = 1;
+	while (args[i] && args[i][0] == '-')
 	{
-		if (args[1][1] == 'n')
-			newline = false;
-		i = 1;
-		while (args[1][++i] && !newline)
+		j = 0;
+		while (args[i][++j])
 		{
-			if (args[1][i] != 'n')
-				newline = true;
+			c = args[i][j];
+			if (c != 'n' && !ft_is_whitespace(c))
+				return (i);
 		}
+		i++;
 	}
-	if (newline)
-		i = 0;
+	return (i);
+}
+
+void	builtin_echo(t_shell *shell, t_ast_node *node)
+{
+	char	**args;
+	char	*arg;
+	size_t	first_arg;
+	t_bool	display_newline;
+
+	args = node->token_node->args;
+	if (!args)
+		return ;
+	first_arg = _determine_first_arg(node);
+	if (first_arg == 1)
+		display_newline = true;
 	else
-		i = 1;
-	while (args[++i])
+		display_newline = false;
+	while (args[first_arg])
 	{
-		arg = env_value_expand(shell, args[i]);
+		arg = env_value_expand(shell, args[first_arg]);
 		ft_putstr_fd(arg, STDOUT_FILENO);
-		if (args[i + 1])
+		if (args[first_arg + 1])
 			ft_putstr_fd(" ", STDOUT_FILENO);
 		free(arg);
+		first_arg++;
 	}
-	if (newline)
+	if (display_newline)
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	shell->exit_code = 0;
 }
